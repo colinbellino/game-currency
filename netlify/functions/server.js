9598463397aa4acddba9b9dde63f7464d72276d3
Colin Bellino <bellino.colin@gmail.com>
2021-05-26 17:07:49 +0200

    Fix currency calculations

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,23 +1,18 @@
-// TODO:
-// Parse input (amount, source, target)
-//
-// Do calculations
-// Inject the result in HMTL
-// Send HTML in response
-
 // Placeholder data from: https://www.reddit.com/r/gaming/comments/725t5v/exchange_rates_of_video_game_currencies/
+// FIXME: These rates are in € but we use them as if they were in USD !
 const currencies = [
-    { id: 0, name: "Euro", rate: 1.0 },
-    { id: 1, name: "US Dollar", rate: 0.83675 },
+    { id: 0, name: "US Dollar", rate: 1.0 },
+    { id: 1, name: "Euro", rate: 1.223093 },
     { id: 2, name: "Mineral (StarCraft)", rate: 34722222.22222 },
-    { id: 3, name: "Coin (Super Mario Bros.)", rate: 100000.0 },
+    { id: 3, name: "Coin (Super Mario Bros.)", rate: 100000 },
     { id: 4, name: "Poke Dollar (Pokémon Red/Blue)", rate: 0.00049 },
+    { id: 5, name: "Rupee (Zelda)", rate: 0.09633 },
 ];
 
 const handler = async(event, context, callback) => {
-    let amount = 100;
-    let source = 2;
-    let target = 3;
+    let amount = 1;
+    let source = 0;
+    let target = 4;
 
     if (event.body) {
         const params = event.body.split("&");
@@ -27,9 +22,9 @@ const handler = async(event, context, callback) => {
         console.log("-> values received:", { source, target });
     }
 
-    const result = amount * source * target;
+    const result = amount * currencies[source].rate / currencies[target].rate;
 
-    console.log({ amount, source, target, result });
+    // console.log({ amount, source, target, result });
 
     return {
         statusCode: 200,
@@ -52,18 +47,14 @@ function renderHTML(amount, source, target, result) {
       <body>
         <h1>Video game currency converter</h1>
 
-<pre>amount: ${amount}
-source: ${JSON.stringify(currencies[source])}
-target: ${JSON.stringify(currencies[target])}
-result: ${result}
-</pre>
-
         <form method="post">
           <input type="number" name="amount" value="${amount}">
           ${renderCurrencySelect("source", source)}
           ${renderCurrencySelect("target", target)}
           <button type="submit">submit</button>
         </form>
+
+        <h2>${amount} ${currencies[source].name} = ${result} ${currencies[target].name}</h2>
       </body>
 
     </html>
