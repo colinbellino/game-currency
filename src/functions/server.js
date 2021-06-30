2076b472cb7690b3e0ba44e06bf5dd83821b5b56
Colin Bellino <bellino.colin@gmail.com>
2021-06-30 17:57:37 +0200

    Swap currencies button style (mobile)

--- src/functions/server.js
+++ src/functions/server.js
@@ -82,13 +82,16 @@ function renderHTML(error, { originalAmount, amount, source, target, result }) {
               <input type="text" name="amount" value="${originalAmount}">
             </label>
             ${renderCurrencySelect("source", source, "From", currencies)}
+            <button type="submit" formaction="/.netlify/functions/swap_currencies" class="swap-button" aria-labelledby="swap-button-label"> 
+              <span id="swap-button-label" hidden>Swap currencies</span>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M388.9 266.3c-5.1-5-5.2-13.3-.1-18.4L436 200H211c-7.2 0-13-5.8-13-13s5.8-13 13-13h224.9l-47.2-47.9c-5-5.1-5-13.3.1-18.4 5.1-5 13.3-5 18.4.1l69 70c1.1 1.2 2.1 2.5 2.7 4.1.7 1.6 1 3.3 1 5 0 3.4-1.3 6.6-3.7 9.1l-69 70c-5 5.2-13.2 5.3-18.3.3zm-265.8 138c5.1-5 5.2-13.3.1-18.4L76.1 338H301c7.2 0 13-5.8 13-13s-5.8-13-13-13H76.1l47.2-47.9c5-5.1 5-13.3-.1-18.4-5.1-5-13.3-5-18.4.1l-69 70c-1.1 1.2-2.1 2.5-2.7 4.1-.7 1.6-1 3.3-1 5 0 3.4 1.3 6.6 3.7 9.1l69 70c5 5.2 13.2 5.3 18.3.3z"/></svg>
+            </button> 
             ${renderCurrencySelect("target", target, "To", currencies)}
             ${error ?
               `<h2 class="error">${error.message}</h2>` :
               `<h2>${renderResult(currencies[source], currencies[target], amount, result)}`
             }
-            <button type="submit">Convert</button>
-            <button type="submit" formaction="/.netlify/functions/swap_currencies">Swap currency</button>
+            <button type="submit" class="convert-button">Convert</button>
           </form>
         </section>
         <script type="module" src="/public/app.js"></script>
