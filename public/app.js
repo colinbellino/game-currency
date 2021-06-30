76a3c9d35e1f97407e2af8814cc92cc4fba155f0
Colin Bellino <bellino.colin@gmail.com>
2021-06-30 18:51:34 +0200

    Swap currencies on the client side
    Closes #9

--- public/app.js
+++ public/app.js
@@ -2,42 +2,43 @@ import { currencies, parseData, validate, renderResult, calculateResult, getCurr
 
 {
   const amountInput = document.querySelector("[name='amount']");
-  const sourceInput = document.querySelector("[name='source']");
-  const targetInput = document.querySelector("[name='target']");
-  const resultElement = document.querySelector("h2");
+  const sourceSelect = document.querySelector("[name='source']");
+  const targetSelect = document.querySelector("[name='target']");
+  const resultHeading = document.querySelector("h2");
+  const swapButton = document.querySelector(".swap-button");
 
-  amountInput.addEventListener("input", calculateAndUpdateResult);
-  sourceInput.addEventListener("input", calculateAndUpdateResult);
-  sourceInput.addEventListener("input", updateSelectImage);
-  targetInput.addEventListener("input", calculateAndUpdateResult);
-  targetInput.addEventListener("input", updateSelectImage);
+  amountInput.addEventListener("input", onAmountChange);
+  sourceSelect.addEventListener("input", onCurencyChange);
+  targetSelect.addEventListener("input", onCurencyChange);
+  swapButton.addEventListener("click", onCurrencySwap);
 
-  function extractData() {
-    const inputs = {
-      amount: amountInput.value,
-      source: sourceInput.value,
-      target: targetInput.value,
-    };
-
-    return [null, inputs];
+  function onAmountChange(_event) {
+    calculateAndUpdateResult();
   }
 
-  function displayError({ message }) {
-    resultElement.classList.add("error");
-    resultElement.innerHTML = message;
+  function onCurencyChange(event) {
+    updateSelectImage(event.target);
+    calculateAndUpdateResult();
   }
 
-  function displayResult({ originalAmount, amount, result, source, target }) {
-    resultElement.classList.remove("error");
-    resultElement.innerHTML = renderResult(currencies[source], currencies[target], amount, result);
+  function onCurrencySwap(event) {
+    const initialTarget = targetSelect.value;
+    targetSelect.value = sourceSelect.value;
+    sourceSelect.value = initialTarget;
+
+    updateSelectImage(targetSelect);
+    updateSelectImage(sourceSelect);
+    calculateAndUpdateResult();
+
+    event.preventDefault();
   }
 
-  function updateSelectImage(event) {
-    var imageElement = event.target.closest(".select-with-icon").querySelector(".select-icon");
-    imageElement.setAttribute("src", getCurrencyURL(currencies[event.target.value]));
+  function updateSelectImage(selectElement) {
+    const imageElement = selectElement.closest(".select-with-icon").querySelector(".select-icon");
+    imageElement.setAttribute("src", getCurrencyURL(currencies[selectElement.value]));
   }
 
-  function calculateAndUpdateResult(_event) {
+  function calculateAndUpdateResult() {
     const [_, inputs] = extractData();
 
     const [parseError, data] = parseData(inputs);
@@ -55,5 +56,25 @@ import { currencies, parseData, validate, renderResult, calculateResult, getCurr
     data.result = calculateResult(data.amount, currencies[data.source].rate, currencies[data.target].rate);
 
     displayResult(data);
+  }
+
+  function extractData() {
+    const inputs = {
+      amount: amountInput.value,
+      source: sourceSelect.value,
+      target: targetSelect.value,
+    };
+
+    return [null, inputs];
+  }
+
+  function displayError({ message }) {
+    resultHeading.classList.add("error");
+    resultHeading.innerHTML = message;
+  }
+
+  function displayResult({ originalAmount, amount, result, source, target }) {
+    resultHeading.classList.remove("error");
+    resultHeading.innerHTML = renderResult(currencies[source], currencies[target], amount, result);
   }
 }
